6937f6f43585a9fb095983daf24afcf2a0bb8eeb
Vojdan Stefkovski <vojdan.stefkovski@students.finki.ukim.mk>
2023-04-06 10:52:03 +0200

    Add project files.

---
+++ Spice/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Spice.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spice.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<SubCategory> SubCategory { get; set; }
+
+    }
+}
